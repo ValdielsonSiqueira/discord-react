e68e433f8cb63b48fbb354054a6cc1e7c0c1aa94
Valdi Siqueira <vsiqueira@fesctecnologia.com.br>
2020-07-19 22:08:49 -0300

    first commit

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Grid } from "./styles";
+
+const Layout: React.FC = () => {
+  return <Grid></Grid>;
+};
+
+export default Layout;
---
+++ src/components/ServerList/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  grid-area: SL;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  background-color: var(--tertiary);
+  padding: 11px 0;
+
+  max-height: 100vh;
+`;
+export const Separator = styled.div``;
