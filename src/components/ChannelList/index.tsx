24dc1c232d107e10ef258734977b3e725533fdc0
Valdi Siqueira <vsiqueira@fesctecnologia.com.br>
2020-07-21 00:51:33 -0300

    feat(product): created component channellist

---
+++ src/components/ChannelList/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Container, Category, AddCategoryIcon } from "./styles";
+
+const ChannelList: React.FC = () => {
+  return (
+    <Container>
+      <Category>
+        <span>Canais de texto</span>
+        <AddCategoryIcon />
+      </Category>
+
+      {/* <ChannelButton channelName="chat-livre" />
+      <ChannelButton channelName="oni-playing" />
+      <ChannelButton channelName="negro-drama" />
+      <ChannelButton channelName="tarzamazonico" />
+      <ChannelButton channelName="obs-studio" /> */}
+    </Container>
+  );
+};
+
+export default ChannelList;
